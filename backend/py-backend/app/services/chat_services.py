3f3f2af96c2b53a74aaea0f5ae54cbfb95e85c33
ujjwal G <crystalninja0123@gmail.com>
2025-11-11 10:04:35 +0530

    chat bot working

--- backend/py-backend/app/services/chat_services.py
+++ backend/py-backend/app/services/chat_services.py
@@ -1,10 +1,24 @@
+
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 import os
+from openai import OpenAI
 load_dotenv()
 
-open_api_key=os.environ["OPEN_AI_KEY"] 
-model = init_chat_model("gpt-4.1", api_key=open_api_key)
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.environ["HF_TOKEN"],
+)
 def generate_response_from_model(message: str) -> str:
-    response = model.invoke(message)
-    return str(response) 
+    try:
+        completion = client.chat.completions.create(
+            model="moonshotai/Kimi-K2-Thinking:novita",
+            messages=[
+                {"role": "user", "content": message}
+            ],
+        )
+
+        return completion.choices[0].message.content
+
+    except Exception as e:
+        return f"Error: {str(e)}"
